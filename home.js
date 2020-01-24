cf72456c2e42239d73223cb74e87f742fcb79802
vaniaraissa <54266849+vaniaraissa@users.noreply.github.com>
2020-01-24 14:20:09 +0700

    Create home.js

---
+++ home.js
@@ -0,0 +1,40 @@
+var pages = [
+
+"https://carabelipulsa.blogspot.com",
+"https://carabelipulsalewatinternet.blogspot.com",
+"https://carabelipulsamurah.blogspot.com",
+"https://carabelipulsaonline.blogspot.com",
+"https://carabelipulsaonlinetelkomsel.blogspot.com",
+"https://carabelipulsatelkomsel.blogspot.com",
+"https://carabelipulsatelkomselonline.blogspot.com",
+"https://carabelitokenlistrikonline.blogspot.com",
+"https://caraberjualanpulsaalloperator.blogspot.com",
+"https://caraberjualanpulsaonline.blogspot.com",
+"https://caradaftarjualpulsa.blogspot.com",
+"https://caradapatpulsagratisalloperatorgratis.blogspot.com",
+"https://caradepositpulsa.blogspot.com",
+"https://carajadiagenpulsaelektrik.blogspot.com",
+"https://carajadidealerpulsa.blogspot.com",
+"https://carajadidistributorpulsa.blogspot.com",
+"https://carajualbelipulsa.blogspot.com",
+"https://carajualpulsaelektrikmurah.blogspot.com",
+"https://caramembelipulsalewatinternet.blogspot.com",
+"https://caramembelipulsa.blogspot.com",
+"https://caramembelipulsaonline.blogspot.com",
+"https://caramenjadimasterdealerpulsa.blogspot.com",
+"https://caramenjadimasterpulsa.blogspot.com",
+"https://caramenjadiserverpulsa.blogspot.com",
+"https://daftarcarajualanpulsa.blogspot.com",
+"https://carausahajualpulsa.blogspot.com",
+"https://cariagenpulsamurah.blogspot.com",
+"https://caripulsadiinternet.blogspot.com",
+"https://cekppob.blogspot.com",
+"https://cetakpulsa.blogspot.com",
+"https://copulsa.blogspot.com",
+"https://customerpulsa.blogspot.com",
+
+];
+function randomPage() {
+return pages[Math.round(Math.random() * (pages.length - 1))];
+}
+location.href= randomPage();
